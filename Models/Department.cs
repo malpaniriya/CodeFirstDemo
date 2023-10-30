2e3ceb3ed7c01b2f2a80026ee25c0647ba602a35
malpaniriya <riyamalpani75@gmail.com>
2023-10-30 16:31:20 +0530

    Add project files.

---
+++ Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeFirstDemo.Models
+{
+    public class Department
+    {
+        public int DeptId { get; set; }
+
+        public string? DeptName { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstDemo.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+
+        public string? Name { get; set; }
+        [Required]
+
+        public string? Email { get; set; }
+
+        //public string?City { get; set; }
+
+        [Required]
+
+        public decimal Salary {get; set; }
+
+        [ForeignKey("DeptId")]
+        public int DeptId { get; set; }
+
+
+    }
+}
